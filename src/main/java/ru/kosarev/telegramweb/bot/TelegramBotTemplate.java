484d4f8251d08988ba64c55e719eafe03289510c
Foll0wTh3Wh1teRabbit <n.kosarev@g.nsu.ry>
2025-08-04 16:48:10 +0700

    Implemented MVP

--- src/main/java/ru/kosarev/telegramweb/bot/TelegramBotTemplate.java
+++ src/main/java/ru/kosarev/telegramweb/bot/TelegramBotTemplate.java
@@ -2,9 +2,6 @@ package ru.kosarev.telegramweb.bot;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.longpolling.interfaces.LongPollingUpdateConsumer;
 import org.telegram.telegrambots.longpolling.starter.SpringLongPollingBot;
 import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
@@ -12,15 +9,12 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.kosarev.telegramweb.processor.TelegramControllerMethodRegistrar;
 
 @Slf4j
-@Component
-@ConditionalOnProperty(prefix = "spring.telegram", name = "bot-token")
 @RequiredArgsConstructor
 public class TelegramBotTemplate implements SpringLongPollingBot, LongPollingSingleThreadUpdateConsumer {
 
-    private final TelegramControllerMethodRegistrar methodRegistrar;
+    private final String botToken;
 
-    @Value("${spring.telegram.bot-token}")
-    private String botToken;
+    private final TelegramControllerMethodRegistrar methodRegistrar;
 
     @Override
     public void consume(Update update) {
--- src/main/java/ru/kosarev/telegramweb/configuration/TelegramWebConfiguration.java
+++ src/main/java/ru/kosarev/telegramweb/configuration/TelegramWebConfiguration.java
@@ -1,20 +1,30 @@
 package ru.kosarev.telegramweb.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import ru.kosarev.telegramweb.bot.TelegramBotTemplate;
 import ru.kosarev.telegramweb.processor.TelegramControllerAnnotationProcessor;
 import ru.kosarev.telegramweb.processor.TelegramControllerMethodRegistrar;
 
 @Configuration
 public class TelegramWebConfiguration {
 
+    @Value("${spring.telegram.bot-token}")
+    private String botToken;
+
     @Bean
-    public TelegramControllerAnnotationProcessor telegramControllerAnnotationProcessor() {
+    public TelegramBotTemplate telegramBotTemplate() {
+        return new TelegramBotTemplate(botToken, methodRegistrar());
+    }
+
+    @Bean
+    public static TelegramControllerAnnotationProcessor telegramControllerAnnotationProcessor() {
         return new TelegramControllerAnnotationProcessor(methodRegistrar());
     }
 
     @Bean
-    public TelegramControllerMethodRegistrar methodRegistrar() {
+    public static TelegramControllerMethodRegistrar methodRegistrar() {
         return new TelegramControllerMethodRegistrar();
     }
 
